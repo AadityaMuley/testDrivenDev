ed4fb3ac6b9ed45b41594beda12d6bc961a7a6a5
Aaditya Muley <aadityamuley8190@gmail.com>
2022-10-27 20:05:16 -0700

    Test added to check if any string if NULL

--- src/test/java/UrinalCheckTest.java
+++ src/test/java/UrinalCheckTest.java
@@ -11,15 +11,20 @@ class UrinalCheckTest {
 
     @Test
     void checkUrinals() {
+        //check if there are consecutive 1's in the string
         assertEquals(-1, UrinalCheck.checkUrinals("011"));
 
-        boolean validate;
+        //check if the string has any other char except 1 or 0
+        boolean validate = false;
         for(int i=0; i<UrinalCheck.data.length(); i++) {
             validate = false;
             if(UrinalCheck.data.charAt(i) == '0' || UrinalCheck.data.charAt(i) == '1') {
                 validate = true;
             }
-            assertEquals(true, validate);
         }
+        assertEquals(true, validate);
+
+        //check if string is not null
+        assertNotEquals(null, UrinalCheck.data);
     }
 }
