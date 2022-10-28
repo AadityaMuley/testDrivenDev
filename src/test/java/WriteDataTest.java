c2d926c0a28fcfed29b69d0f9e711c7007f8babc
Aaditya Muley <aadityamuley8190@gmail.com>
2022-10-27 22:16:53 -0700

    Test added to check if the path name if valid

--- src/test/java/WriteDataTest.java
+++ src/test/java/WriteDataTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,10 +13,24 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class WriteDataTest {
 
+    public static boolean pathValidity(String tempPath) {
+        try{
+            Paths.get(tempPath);
+        }
+        catch (InvalidPathException | NullPointerException ex) {
+            return false;
+        }
+        return true;
+    }
+
     @Test
     void writeFile() {
+        //check if file already exists
         String tempPath = WriteData.path + WriteData.fileNum + ".txt";
         WriteData.file = new File(tempPath);
         assertEquals(false, WriteData.file.exists());
+
+        //check if file name is proper format
+        assertEquals(true, pathValidity(tempPath));
     }
 }
